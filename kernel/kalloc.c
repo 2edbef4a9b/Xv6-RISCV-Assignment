71a527e4a0d554001b4dc22691c04bc375dd1cb6
2edbef4a9b <echofedn@outlook.com>
2025-07-23 12:30:31 +0800

    Implement normal page merging

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -19,7 +19,7 @@ extern char end[]; // first address after kernel.
 
 // Free page count for each super page.
 struct {
-  uint8 count;
+  uint count;
   struct spinlock lock;
 } freecount[SUPERPGCOUNTS];
 
@@ -48,6 +48,27 @@ freerange(void *pa_start, void *pa_end)
   struct run *r;
 
   p = (char*)PGROUNDUP((uint64)pa_start);
+
+  // Add normal pages to the freelist until we reach the super page boundary.
+  for(; p + PGSIZE <= (char*)SUPERPGROUNDUP((uint64)pa_end); p += PGSIZE){
+    // Fill with junk to catch dangling refs.
+    memset(p, 1, PGSIZE);
+    r = (struct run*)p;
+    acquire(&kmem.lock);
+    if(kmem.freelist)
+      kmem.freelist->prev = r;
+    r->next = kmem.freelist;
+    r->prev = 0;
+    kmem.freelist = r;
+    release(&kmem.lock);
+
+    // Update free page count for this super page.
+    acquire(&freecount[USAGEIDX((uint64)p)].lock);
+    freecount[USAGEIDX((uint64)p)].count++;
+    release(&freecount[USAGEIDX((uint64)p)].lock);
+  }
+
+  // Add super pages to the superlist.
   for(; p + SUPERPGSIZE <= (char*)pa_end; p += SUPERPGSIZE){
     // Fill with junk to catch dangling refs.
     memset(p, 1, SUPERPGSIZE);
@@ -62,10 +83,11 @@ freerange(void *pa_start, void *pa_end)
 
     // Update free page count for this super page.
     acquire(&freecount[USAGEIDX((uint64)p)].lock);
-    freecount[USAGEIDX((uint64)p)].count = (uint8)SUPERPGSIZE / PGSIZE;
+    freecount[USAGEIDX((uint64)p)].count = 512;
     release(&freecount[USAGEIDX((uint64)p)].lock);
   }
 
+  // Add remaining normal pages to the freelist.
   for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE){
     // Fill with junk to catch dangling refs.
     memset(p, 1, PGSIZE);
@@ -113,6 +135,42 @@ ksplit()
   }
 }
 
+// Merge normal pages into a super page that contains pa.
+void
+kmerge(uint64 pa)
+{
+  struct run *r;
+  uint64 start;
+
+  start = SUPERPGROUNDDOWN(pa);
+  acquire(&freecount[USAGEIDX(pa)].lock);
+  if(freecount[USAGEIDX(pa)].count != 512){
+    release(&freecount[USAGEIDX(pa)].lock);
+    return; // Not enough free pages to merge.
+  }
+  release(&freecount[USAGEIDX(pa)].lock);
+
+  acquire(&kmem.lock);
+  for(pa = start; pa < start + SUPERPGSIZE; pa += PGSIZE){
+    r = (struct run*)pa;
+    if(r->prev)
+      r->prev->next = r->next;
+    else
+      kmem.freelist = r->next;
+    if(r->next)
+      r->next->prev = r->prev;
+  }
+
+  // Add the super page to the superlist.
+  r = (struct run*)start;
+  if(kmem.superlist)
+    kmem.superlist->prev = r;
+  r->next = kmem.superlist;
+  r->prev = 0;
+  kmem.superlist = r;
+  release(&kmem.lock);
+}
+
 // Free the page of physical memory pointed at by pa,
 // which normally should have been returned by a
 // call to kalloc().  (The exception is when
@@ -142,6 +200,9 @@ kfree(void *pa)
   acquire(&freecount[USAGEIDX((uint64)pa)].lock);
   freecount[USAGEIDX((uint64)pa)].count++;
   release(&freecount[USAGEIDX((uint64)pa)].lock);
+
+  // Try merging with the super page.
+  kmerge((uint64)pa);
 }
 
 // Allocate one 4096-byte page of physical memory.
